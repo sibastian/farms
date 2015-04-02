16f81635ef65fddfe3df0595da51b4fbb7d129cc
Sankar Gurung <sibastiangurung@gmail.com>
2015-04-02 17:33:21 +0545

    First Change

---
+++ s.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name          	PATCH
+//
+// @license         GPLv3 - http://www.gnu.org/licenses/gpl-3.0.txt
+// @copyright       Copyright (C) 2012, by Sepehr Lajevardi <me@sepehr.ws>
+//
+// @include         http://psamsdev01.farrms.us/*
+// @require         http://code.jquery.com/jquery-1.8.0.min.js
+// @version         1.0
+
+// @run-at			document-start|document-end
+// @resource		css	localhost:8080/farms/css/patch.css
+
+// ==/UserScript==
+
+
+
+(function() {	
+	
+
+
+$('head').append('<link rel="stylesheet" href="http://localhost:8080/farms/css/patch.css" type="text/css"/>');
+
+
+	
+})();
