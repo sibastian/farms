1a607e56659f7aea9178ece7256060d62773f0ad
adishaswot <adishaswot@gmail.com>
2015-04-03 20:02:14 +0545

    patch edited

---
+++ patch.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name          	PATCH
+//
+// @license         GPLv3 - http://www.gnu.org/licenses/gpl-3.0.txt
+// @copyright       Copyright (C) 2012, by Sepehr Lajevardi <me@sepehr.ws>
+//
+// @include         http://psamsdev01.farrms.us/*
+// @require         http://code.jquery.com/jquery-1.8.0.min.js
+// @version         1.0
+
+// @run-at			document-start|document-end
+// @resource		css	localhost:8080/farms/css/patch.css
+
+// ==/UserScript==
+
+
+
+(function() {	
+	
+
+//Sebastain
+// $('head').append('<link rel="stylesheet" href="http://localhost:8080/farms/css/patch.css" type="text/css"/>');$('head').append('<link rel="stylesheet" href="http://localhost/farms/css/patch.css" type="text/css"/>');
+
+//Addy
+$('head').append('<link rel="stylesheet" href="http://localhost/farms/css/patch.css" type="text/css"/>');$('head').append('<link rel="stylesheet" href="http://localhost/farms/css/patch.css" type="text/css"/>');
+
+
+//Client
+$('head').append('<link rel="stylesheet" href="http://thedesignpig.com/d/farms/css/patch.css" type="text/css"/>');$('head').append('<link rel="stylesheet" href="http://localhost/farms/css/patch.css" type="text/css"/>');
+
+
+
+
+	
+})();
